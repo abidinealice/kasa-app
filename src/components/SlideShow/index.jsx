22475da88519193196ef41c8c01ff084c51c2723
Alice Abidine <abidinealice@gmail.com>
2024-02-18 23:49:30 +0100

    màj
    >slideshow component

--- src/components/SlideShow/index.jsx
+++ src/components/SlideShow/index.jsx
@@ -1,5 +1,20 @@
+import Example from '../../assets/Example.png'
+import ArrowLeft from '../../assets/ArrowLeft.png'
+import ArrowRight from '../../assets/ArrowRight.png'
+
 function SlideShow() {
-  return <div>Cest le composant ss</div>
+  return (
+    <div className="SlideShowContainer">
+      <img src={Example} alt="example" className="SlideShowCar"></img>
+      <button className="SlideShowButtonL">
+        <img src={ArrowLeft} alt="Fleche gauche" />
+      </button>
+      <button className="SlideShowButtonR">
+        <img src={ArrowRight} alt="Fleche droit" />
+      </button>
+      <span className="SlideShowCounter">1/4</span>
+    </div>
+  )
 }
 
 export default SlideShow
